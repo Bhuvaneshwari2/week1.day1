675d9de74402cdc9f93580ccdbc564a3fd9b9d9c
Bhuvaneshwari2 <94557977+Bhuvaneshwari2@users.noreply.github.com>
2021-11-17 23:01:57 +0530

    Add files via upload

---
+++ LearnArray.java
@@ -0,0 +1,39 @@
+package week1.day.sheriba;
+
+import java.util.Arrays;
+
+public class LearnArray {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//     int intArray[]= {23,56,82,104,78};
+//	  int lenArray=intArray.length;
+//	  for(int i=0;i<lenArray;i++) {
+//	   Arrays.sort(intArray);//ascending order
+//			System.out.println("smallest number in array:"+ intArray[i]);
+//	  }
+//			System.out.println("largest number in array:"+ intArray[lenArray-1]);	   
+//	}
+//
+//}
+//		System.out.println(*******************************************************);
+		//reverse(Decreasing)incomplete
+//	  int intArray[]= {23,56,82,104,78};
+//	  int lenArray=intArray.length;
+//		 for(int i=lenArray;i>=1;i--) {
+//			 System.out.println(intArray[i]);
+//		 }}}
+//System.out.println(********************************************************);
+           //Ascending order
+//		 int intArray[]= {23,56,82,104,78};
+//		  int lenArray=intArray.length;
+//		  Arrays.sort(intArray);
+//		  System.out.println("sorted Array");
+//			 for(int i=0;i<lenArray;i++) {
+//				 System.out.println(intArray[i]);
+//			 }}}
+	}}
+
+
+
+
+
